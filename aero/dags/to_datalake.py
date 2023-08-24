6870c20ab7cb4323441d40e8955a18a2538a4a50
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-08-24 23:04:19 +0200

    Added to_datalake DAG

--- aero/dags/to_datalake.py
+++ aero/dags/to_datalake.py
@@ -1,6 +1,7 @@
 from airflow.decorators import task, dag
-from airflow.exceptions import AirflowSkipException
 from airflow.utils.trigger_rule import TriggerRule
+
+from resources.datalake.resource import S3DataLakeResource
 
 import pendulum
 import datetime
@@ -8,21 +9,64 @@ import datetime
 
 @task()
 def e_from_api_datasource() -> dict:
-    from req
+    import pandas as pd
+    import requests
 
+    response = requests.get('https://random-data-api.com/api/cannabis/random_cannabis?size=10').json()
+    df = pd.DataFrame(data=response)
+
+    return df.to_dict(orient='list')
+
+
+@task(trigger_rule=TriggerRule.ALL_SUCCESS)
+def l_to_datalake(df: dict, s3: S3DataLakeResource, dt: datetime.datetime) -> None:
+    import pandas as pd
+
+    dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+    df = pd.DataFrame.from_dict(df)
+    df['ts'] = dt
+
+    df.rename(
+        columns={
+            "id": 'dm_cannabis_id',
+            "uid": "dm_cannabis_uid",
+            "strain": "dm_cannabis_strain",
+            "cannabinoid_abbreviation": "dm_cannabis_cannabinoid_abbreviation",
+            "cannabinoid": "dm_cannabis_cannabinoid",
+            "terpene": "dm_cannabis_terpene",
+            "medical_use": "dm_cannabis_medical_use",
+            "health_benefit": "dm_cannabis_health_benefit",
+            "category": "dm_cannabis_category",
+            "type": "dm_cannabis_type",
+            "buzzword": "dm_cannabis_buzzword",
+            "brand": "dm_cannabis_brand",
+            'ts': 'dm_cannabis_timestamp'
+        },
+        inplace=True
+    )
+
+    s3.to_csv(df=df, path=f"s3://datalake-bucket/cannabis/{dt}")
 
 
 @dag(
     dag_id='to_datalake',
     start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
-    schedule_interval='*/10 * * * *',
+    schedule_interval='*/1 * * * *',
     catchup=False,
     max_active_runs=1,
-    dagrun_timeout=datetime.timedelta(minutes=10)
+    dagrun_timeout=datetime.timedelta(minutes=1)
 )
 def to_datalake():
     from resources.datalake.resource import s3_datalake
-    from resources.datamart.resource import ch_datamart
+
+    s3, now = s3_datalake(), datetime.datetime.now()
+
+    df = e_from_api_datasource()
+    l_to_datalake(
+        df=df,
+        s3=s3,
+        dt=now
+    )
 
 
 to_datalake_dag = to_datalake()
