e937889e06f918a0c4de6c8b26512de72b62cc17
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-03 20:48:03 +0100

    Updated settings

--- {{cookiecutter.project}}/app/settings.py
+++ {{cookiecutter.project}}/app/settings.py
@@ -2,7 +2,7 @@ from distutils.util import strtobool
 import os
 
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 from app.utils import initialize_class, call_function
 
