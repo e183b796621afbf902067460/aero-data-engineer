d2e0ad6e1c900f6695cecb5a21cf5a767756a058
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-04 22:53:07 +0100

    Added to_clickhouse method to FastKafkaApp

--- {{cookiecutter.project}}/app/streaming.py
+++ {{cookiecutter.project}}/app/streaming.py
@@ -1,5 +1,9 @@
+import asyncio
+
 from fastkafka import FastKafka
 
+from app.adapters.connections.kafka.producer import AIOKafkaProducerConnection
+from app.schemas.transactions.schema import TransactionsBatch
 from app.settings import settings
 from app.utils import initialize_class
 
@@ -26,6 +30,31 @@ class FastKafkaApp(FastKafka):
             *args,
             **kwargs,
         )
+
+    @staticmethod
+    async def to_clickhouse(producer: AIOKafkaProducerConnection, events: TransactionsBatch) -> None:
+        """Asynchronously send real-time transactions to ClickHouse integrated with Kafka engine.
+
+        Args:
+        ----
+            producer (AIOKafkaProducerConnection): A Kafka producer connection.
+            events (TransactionsBatch): Batch of real-time transactions events.
+
+        Returns:
+        -------
+            None
+
+        Note:
+        ----
+            This method uses the provided Kafka producer to send each transaction series
+            in the provided batch to the Kafka topic.
+        """
+        await asyncio.gather(
+            *(
+                producer.send(topic=settings.TOPIC_NAME, value=dict(series))
+                for series in events.q_real_time_tx_processing_series
+            ),
+        ) if events.q_real_time_tx_processing_series else ...
 
 
 fastkafka_app = initialize_class(FastKafkaApp)
