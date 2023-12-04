16ed4fe3dd730a011f49ceb3e0fa4aa79ba1def9
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-04 22:21:59 +0100

    Updated providers

--- {{cookiecutter.project}}/app/adapters/connections/providers/http.py
+++ {{cookiecutter.project}}/app/adapters/connections/providers/http.py
@@ -4,7 +4,15 @@ from app.settings import settings
 
 
 class HTTPProviderConnection(HTTPProvider):
-    address, is_reverse = None, None
+    """HTTPProviderConnection extends HTTPProvider to establish a connection
+    to a remote HTTP provider with the specified URI.
+
+    Note:
+    ----
+        This class initializes the HTTPProvider with default values,
+        setting the URI to the value specified in the 'WSS_NODE_PROVIDER'
+        setting from the application settings.
+    """
 
     def __init__(self):
-        HTTPProvider.__init__(self, uri=settings.WSS_NODE_PROVIDER)
+        HTTPProvider.__init__(self, uri=settings.HTTP_NODE_PROVIDER)
