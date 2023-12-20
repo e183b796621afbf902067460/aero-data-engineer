c7650ac031504e5d34975972478176b5aa08d110
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-21 00:58:37 +0100

    Updated tests

--- {{cookiecutter.project}}/tests/unit/adapters/repositories/wss_repository/test_decimals.py
+++ {{cookiecutter.project}}/tests/unit/adapters/repositories/wss_repository/test_decimals.py
@@ -10,24 +10,24 @@ except ImportError:
 
 
 # TODO implement particular websocket repository type hints
-@pytest.mark.repositories
+@pytest.mark.unit
 def test__wss_repository__token0_decimals__must_be_int(wss_repository: iRepository):  # noqa: D103
     assert isinstance(wss_repository._token0_decimals, int)
 
 
 # TODO implement particular websocket repository type hints
-@pytest.mark.repositories
+@pytest.mark.unit
 def test__wss_repository__token1_decimals__must_be_int(wss_repository: iRepository):  # noqa: D103
     assert isinstance(wss_repository._token1_decimals, int)
 
 
 # TODO implement particular websocket repository type hints and fill to_ option
-@pytest.mark.repositories
+@pytest.mark.unit
 def test__wss_repository__token0_decimals__must_be_equal_to_(wss_repository: iRepository):  # noqa: D103
     assert wss_repository._token0_decimals == ...
 
 
 # TODO implement particular websocket repository type hints and fill to_ option
-@pytest.mark.repositories
+@pytest.mark.unit
 def test__wss_repository__token1_decimals__must_be_equal_to_(wss_repository: iRepository):  # noqa: D103
     assert wss_repository._token1_decimals == ...
