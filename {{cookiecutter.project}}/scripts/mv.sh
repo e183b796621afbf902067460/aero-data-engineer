ba0826071ae57ca584822670374dda6fdf39a313
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-20 01:41:52 +0100

    Added default .github directory to template

--- {{cookiecutter.project}}/scripts/mv.sh
+++ {{cookiecutter.project}}/scripts/mv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "Moving core files to project's root $(pwd)."
+echo "Moving core files to project's root $(pwd)/."
 
 mv ./{{cookiecutter.project}}/.gitignore $(pwd)/
 mv ./{{cookiecutter.project}}/.pre-commit-config.yaml $(pwd)/
 mv ./{{cookiecutter.project}}/docker-compose.yaml $(pwd)/
 mv ./{{cookiecutter.project}}/README.md $(pwd)/
+mv ./{{cookiecutter.project}}/.github $(pwd)/
