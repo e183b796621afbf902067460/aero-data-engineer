609b69fdc40ba70306abe90952f63031d436f83f
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-12-21 01:57:15 +0100

    Updated mv.sh script

--- {{cookiecutter.project}}/scripts/mv.sh
+++ {{cookiecutter.project}}/scripts/mv.sh
@@ -2,9 +2,58 @@
 
 echo "Moving core files to project's root $(pwd)/."
 
-mv ./{{cookiecutter.project}}/.gitignore $(pwd)/
-mv ./{{cookiecutter.project}}/.pre-commit-config.yaml $(pwd)/
-mv ./{{cookiecutter.project}}/docker-compose.yaml $(pwd)/
-mv ./{{cookiecutter.project}}/README.md $(pwd)/
-mv ./{{cookiecutter.project}}/.github $(pwd)/
-mv ./{{cookiecutter.project}}/_clickhouse $(pwd)/
+# Function to move and check
+move_and_check() {
+    src="$1"
+    dest="$2"
+
+    mv "$src" "$dest"
+    if [ $? -eq 0 ]; then
+        echo "$src moved successfully to $dest [x]."
+    else
+        echo "Failed to move $src to $dest [-]."
+    fi
+    echo
+}
+
+# Move .github to $(pwd)
+move_and_check "./{{cookiecutter.project}}/.github" "$(pwd)/"
+
+# Move _clickhouse to $(pwd)
+move_and_check "./{{cookiecutter.project}}/_clickhouse" "$(pwd)/"
+
+# Move app to $(pwd)
+move_and_check "./{{cookiecutter.project}}/app" "$(pwd)/"
+
+# Move tests to $(pwd)
+move_and_check "./{{cookiecutter.project}}/tests" "$(pwd)/"
+
+# Move .gitignore to $(pwd)
+move_and_check "./{{cookiecutter.project}}/.gitignore" "$(pwd)/"
+
+# Move .pre-commit-config.yaml to $(pwd)
+move_and_check "./{{cookiecutter.project}}/.pre-commit-config.yaml" "$(pwd)/"
+
+# Move docker-compose.yaml to $(pwd)
+move_and_check "./{{cookiecutter.project}}/docker-compose.yaml" "$(pwd)/"
+
+# Move Dockerfile to $(pwd)
+move_and_check "./{{cookiecutter.project}}/Dockerfile" "$(pwd)/"
+
+# Move poetry.lock to $(pwd)
+move_and_check "./{{cookiecutter.project}}/poetry.lock" "$(pwd)/"
+
+# Move pyproject.toml to $(pwd)
+move_and_check "./{{cookiecutter.project}}/pyproject.toml" "$(pwd)/"
+
+# Move README.md to $(pwd)
+move_and_check "./{{cookiecutter.project}}/README.md" "$(pwd)/"
+
+# Remove {{cookiecutter.project}} directory
+echo "Removing {{cookiecutter.project}} directory..."
+rm -rf "{{cookiecutter.project}}"
+if [ $? -eq 0 ]; then
+    echo "{{cookiecutter.project}} directory removed successfully [x]."
+else
+    echo "Failed to remove {{cookiecutter.project}} directory [-]."
+fi
